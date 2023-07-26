20d78f9514c10bf7039edf7e83149095413ab24e
LimJiseong <yim2627@gmail.com>
2023-07-26 15:49:18 +0900

    pkg init

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version: 5.7
+
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+	name: "RangeSeekSlider",
+	products: [
+		.library(
+			name: "RangeSeekSlider",
+			targets: ["RangeSeekSlider"]),
+	],
+	targets: [
+		.target(
+			name: "RangeSeekSlider",
+			path: "Sources")
+	]
 )
